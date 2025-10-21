5173572a48a7563bd5d94136978b4a359caa0f08
hasyimzii <imhasyimari@gmail.com>
2025-10-21 15:37:37 +0700

    feat: init go

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run("localhost:3000")
+}
